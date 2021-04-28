0bc2e90fa4eae802740f885dcc8b181f9361cea5
A-aron Paul Luminding <aaron@cyfusions.com>
2021-04-27 23:15:09 -0400

    Created profile

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Profile
+from .forms import ProfileForm
 
-# Create your views here.
+def my_profile_view(request):
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileForm(request.POST or None, request.FILES or None, instance=profile)
+    confirm = False
+
+    if form.is_valid():
+        form.save()
+        confirm = True
+
+    context = {
+        'profile': profile,
+        'form': form,
+        'confirm': confirm
+    }
+    return render(request, 'profiles/main.html', context)
--- src/sales_reports/urls.py
+++ src/sales_reports/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sales.urls', namespace='sales')),
     path('reports/', include('reports.urls', namespace='reports')),
+    path('my_profile/', include('profiles.urls', namespace='profiles')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
